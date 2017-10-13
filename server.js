ac511d4a4d8145f6f15c430d104a5bc96bbb2ed0
Thomas <bernadet.thomas@yahoo.fr>
2017-10-13 10:45:02 +0200

    @ mise en place config basique de hapi

---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const Hapi = require('hapi');
+const Good = require('good');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+        reply('Hello, worldddd!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{name}',
+    handler: function (request, reply) {
+        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', `Server running at: ${server.info.uri}`);
+    });
+});
